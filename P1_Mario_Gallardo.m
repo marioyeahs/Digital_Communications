2b34f8575dd5923f345c2fa639167fb1612c1c3e
marioyeahs <marioyeahs@gmail.com>
2023-03-05 13:08:08 -0600

    add: filtered signal y(t)

--- P1_Mario_Gallardo.m
+++ P1_Mario_Gallardo.m
@@ -22,20 +22,32 @@ ylabel("A")
 
 [X1,x11,df1]=Fftseq(x1,ts,df);
 X11=X1/fs;
-f=[0:df1:df1*(length(x11)-1)]-fs/2;
+h_t=[0:df1:df1*(length(x11)-1)]-fs/2;
 subplot(3,1,2)
-plot(f,fftshift(abs(X11)))
+plot(h_t,fftshift(abs(X11)))
 xlim([-2 2])
 title("Espectro de magnitud la señal x(t)")
 xlabel("\omega")
 ylabel("|X(\omega)|")
 subplot(3,1,3)
-plot(f(500:525),fftshift(angle(X11(500:525))))
+plot(h_t(500:525),fftshift(angle(X11(500:525))))
 title("Espectro de fase de la señal x(t)")
 xlabel("\omega")
 ylabel("\theta")
 
 sgtitle("Mario A. Gallardo Cervantes")
+ 
+% parte B
+% obtener la señal de salida y(t) si la señal de entrada es x(t) y se le aplica un filtro con respuestal al impulso h(t)
 
-%parte B
+h_t = ((0<=t)&(t<1)).*(t)+((1<=t)&(t<=2)).*1;
+y = filter(h_t, 1, x1);
 
+% figure
+% Visualizar la respuesta en frecuencia del filtro
+% freqz(x1, 1);
+ 
+figure
+plot(t,y)
+xlim([0 5])
+title("Señal x(t) filtrada con h(t)")
